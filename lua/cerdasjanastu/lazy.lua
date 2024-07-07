c830e2c0456293bc3af69ec92c85f1b0b3b25d25
cerdasjanastu <cerdasjanastu@gmail.com>
2024-07-07 14:15:15 +0800

    small update lazy

--- lua/cerdasjanastu/lazy.lua
+++ lua/cerdasjanastu/lazy.lua
@@ -1,17 +1,23 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    spec = "cerdasjanastu.lazy_plugins",
-    change_detection = { notify = false },
+    spec = {
+        import = "cerdasjanastu.lazy_plugins",
+    },
+    defaults = {
+        lazy = false,
+        version = false, 
+    },
+    checker = { enabled = false }, -- automatically check for plugin updates
 })
