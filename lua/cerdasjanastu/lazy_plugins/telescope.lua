8de2a2243e9855b3c98ccbe49e8ceeec7c66c223
cerdasjanastu <cerdasjanastu@gmail.com>
2024-07-07 14:13:26 +0800

    add telescope setup defaults configs and mappings

--- lua/cerdasjanastu/lazy_plugins/telescope.lua
+++ lua/cerdasjanastu/lazy_plugins/telescope.lua
@@ -1,15 +1,32 @@
 return {
     {
         "nvim-telescope/telescope.nvim",
+        name = "telescope",
         tag = "0.1.8",
         dependencies = {
             "nvim-lua/plenary.nvim",
             lazy = true
         },
         config = function()
-            require("telescope").setup{}
+            require("telescope").setup({
+                defaults = {
+                    layout_stategy = "horizontal",
+                    sorting_strategy = "ascending",
+                    layout_config = {
+                        horizontal = {
+                            prompt_position = "top",
+                        }
+                    },
+                    mappings = {
+                        n = {
+                            ["<C-c>"] = "close"
+                        }
+                    }
+                },
+            })
 
             local builtin = require("telescope.builtin")
+
             vim.keymap.set("n", '<leader>pf', builtin.find_files, {})
             vim.keymap.set("n", '<C-p>', builtin.git_files, {})
             vim.keymap.set("n", '<leader>pws', function()
@@ -24,6 +41,7 @@ return {
                 builtin.grep_string({ search = vim.fn.input("Grep > ") })
             end)
             vim.keymap.set("n", '<leader>vh', builtin.help_tags, {})
+            vim.keymap.set("n", "<leader>tel", builtin.builtin, {})
         end
     },
 }
