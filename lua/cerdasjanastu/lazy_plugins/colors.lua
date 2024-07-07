2a9cd1a5cdc762c23d00aae376a15d8097f7a429
cerdasjanastu <cerdasjanastu@gmail.com>
2024-07-07 14:08:17 +0800

    fix color settings

--- lua/cerdasjanastu/lazy_plugins/colors.lua
+++ lua/cerdasjanastu/lazy_plugins/colors.lua
@@ -1,9 +1,9 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine-moon"
-	vim.cmd.colorscheme(color)
+    color = color or "tokyonight"
+    vim.cmd("colorscheme " .. color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
@@ -61,7 +61,7 @@ return {
         "folke/tokyonight.nvim",
         config = function()
             require("tokyonight").setup({
-                style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+                style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
                 transparent = true, -- Enable this to disable setting the background color
                 terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
                 styles = {
