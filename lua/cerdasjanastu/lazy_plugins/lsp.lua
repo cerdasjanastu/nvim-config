a14a08ffab80169f5e676501d67e7e7fd731abda
cerdasjanastu <cerdasjanastu@gmail.com>
2024-07-17 19:09:00 +0800

    small update

--- lua/cerdasjanastu/lazy_plugins/lsp.lua
+++ lua/cerdasjanastu/lazy_plugins/lsp.lua
@@ -83,16 +83,16 @@ return {
                 end,
             },
             mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+                ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+                ["<C-y>"] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
             }),
             sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
+                { name = "nvim_lsp" },
+                { name = "luasnip" }, -- For luasnip users.
             }, {
-                { name = 'buffer' },
+                { name = "buffer" },
             })
         })
 
