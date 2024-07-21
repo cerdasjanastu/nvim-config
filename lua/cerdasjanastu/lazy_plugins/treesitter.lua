dafd0ebe7b8349f9f55875d859a1c929c9c06ab4
cerdasjanastu <cerdasjanastu@gmail.com>
2024-07-21 13:43:50 +0800

    add ensure_installed treesitter

--- lua/cerdasjanastu/lazy_plugins/treesitter.lua
+++ lua/cerdasjanastu/lazy_plugins/treesitter.lua
@@ -1,7 +1,31 @@
 return {
     {
         'nvim-treesitter/nvim-treesitter',
-        build = ":TSUpdate"
+        build = ":TSUpdate",
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+            configs.setup({
+                ensure_installed = {
+                    "lua",
+                    "python",
+                    "json",
+                    "xml",
+                    "html",
+                    "css",
+                    "javascript",
+                    "vim",
+                    "vimdoc",
+                },
+                sync_install = false,
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                },
+                indent = {
+                    enable = true,
+                }
+            })
+        end
     },
 
     {
