ec15140e67f334af2aedda9a2000d742ffd32055
cerdasjanastu <cerdasjanastu@gmail.com>
2024-07-21 13:44:34 +0800

    change harpoon shortcut

--- lua/cerdasjanastu/lazy_plugins/harpoon.lua
+++ lua/cerdasjanastu/lazy_plugins/harpoon.lua
@@ -5,7 +5,7 @@ return {
         dependencies = {
             "nvim-lua/plenary.nvim",
             lazy = true
-        }, 
+        },
         config = function()
             local harpoon = require("harpoon")
 
@@ -14,14 +14,10 @@ return {
             vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
             vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-            vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
-            vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "<leader><C-j>", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "<leader><C-k>", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<leader><C-l>", function() harpoon:list():select(4) end)
+            vim.keymap.set("n", "<A-h>", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<A-j>", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<A-k>", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<A-l>", function() harpoon:list():select(4) end)
 
             -- Toggle previous & next buffers stored within Harpoon list
             vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
