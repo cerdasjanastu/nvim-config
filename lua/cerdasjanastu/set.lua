002fcee479539b31d56bcf1bcef218363e42df1d
cerdasjanastu <cerdasjanastu@gmail.com>
2024-07-22 22:41:47 +0800

    update set

--- lua/cerdasjanastu/set.lua
+++ lua/cerdasjanastu/set.lua
@@ -6,22 +6,27 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
-
 
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.cursorline = false
+
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 10
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
@@ -31,3 +36,15 @@ vim.opt.colorcolumn = "80"
 
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+--
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("cerdas-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
