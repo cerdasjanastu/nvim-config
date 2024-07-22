4f7da6dd73c3e6e5a5557f1143c269086ddc9ad1
cerdasjanastu <cerdasjanastu@gmail.com>
2024-07-22 22:42:18 +0800

    small update

--- lua/cerdasjanastu/remap.lua
+++ lua/cerdasjanastu/remap.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.api.nvim_set_keymap('i', '<C-H>', '<C-W>', {noremap = true})
+vim.api.nvim_set_keymap("i", "<C-H>", "<C-W>", {noremap = true})
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -23,6 +25,5 @@ vim.keymap.set("v", "<leader>d", "\"_d")
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux new tmux sessionizer<CR>")
-
+-- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux new tmux sessionizer<CR>")
 
