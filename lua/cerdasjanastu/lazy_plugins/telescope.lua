68d542cd9cb25f4dcd5b7fc39d9ac7625f5a64ae
cerdasjanastu <cerdasjanastu@gmail.com>
2024-07-22 22:44:18 +0800

    update remap telescope

--- lua/cerdasjanastu/lazy_plugins/telescope.lua
+++ lua/cerdasjanastu/lazy_plugins/telescope.lua
@@ -10,8 +10,11 @@ return {
             },
             {
                 "nvim-telescope/telescope-fzf-native.nvim",
-                build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release"
-            },
+                build = "make",
+                cond = function()
+                    return vim.fn.executable "make" == 1
+                end,
+           },
         },
         config = function()
             require("telescope").setup({
@@ -33,21 +36,41 @@ return {
 
             local builtin = require("telescope.builtin")
 
-            vim.keymap.set("n", '<leader>pf', builtin.find_files, {})
-            vim.keymap.set("n", '<C-p>', builtin.git_files, {})
-            vim.keymap.set("n", '<leader>pws', function()
+            vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>sit", builtin.git_files, {})
+            vim.keymap.set("n", "<leader>sw", function()
                 local word = vim.fn.expand("<cword>")
                 builtin.grep_string({ search = word })
             end)
-            vim.keymap.set("n", '<leader>pWs', function()
+            vim.keymap.set("n", "<leader>sW", function()
                 local word = vim.fn.expand("<cWORD>")
                 builtin.grep_string({ search = word })
             end)
-            vim.keymap.set("n", '<leader>ps', function()
-                builtin.grep_string({ search = vim.fn.input("Grep > ") })
-            end)
-            vim.keymap.set("n", '<leader>vh', builtin.help_tags, {})
-            vim.keymap.set("n", "<leader>tel", builtin.builtin, {})
+            vim.keymap.set("n", "<leader>sg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>sr", builtin.resume, {})
+            vim.keymap.set("n", "<leader>sd", builtin.diagnostics, {})
+            vim.keymap.set("n", '<leader>sh', builtin.help_tags, {})
+            vim.keymap.set("n", "<leader>ss", builtin.builtin, {})
+            vim.keymap.set("n", "<leader>s.", builtin.oldfiles, {})
+            vim.keymap.set("n", "<leader><leader>", builtin.buffers, {})
+
+            vim.keymap.set("n", "<leader>/", function()
+                builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
+                    previewer = false,
+                })
+            end, {})
+
+            vim.keymap.set("n", "<leader>s/", function()
+                builtin.live_grep {
+                    grep_open_files = true,
+                    prompt_title = "Live Grep in Open Files",
+                }
+            end, {})
+
+            vim.keymap.set("n", "<leader>sn", function()
+                builtin.find_files { cwd = vim.fn.stdpath "config" }
+            end, {})
+
         end
     },
 }
