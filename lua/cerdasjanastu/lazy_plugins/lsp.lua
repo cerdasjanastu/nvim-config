afdba4f9da69131accd34872d95aa0a2027a3d00
cerdasjanastu <cerdasjanastu@gmail.com>
2024-11-30 14:58:40 +0800

    update lsp

--- lua/cerdasjanastu/lazy_plugins/lsp.lua
+++ lua/cerdasjanastu/lazy_plugins/lsp.lua
@@ -1,103 +1,127 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        { "williamboman/mason.nvim", config = True },
-        "williamboman/mason-lspconfig.nvim",
-        "j-hui/fidget.nvim",
-        "folke/neodev.nvim",
+    {
+        -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+        -- used for completion, annotations and signatures of Neovim apis
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+            library = {
+                -- Load luvit types when the `vim.uv` word is found
+                { path = "luvit-meta/library", words = { "vim%.uv" } },
+            },
+        },
     },
 
-    config = function()
-        vim.api.nvim_create_autocmd("LspAttach", {
-            group = vim.api.nvim_create_augroup("cerdas-lsp-attach", { clear = true }),
-            callback = function(event)
-                local map = function(keys, func, desc)
-                    vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-                end
+    { "Bilal2453/luvit-meta", lazy = true },
 
-                -- Jump to the definition of the word under your cursor.
-                --  This is where a variable was first declared, or where a function is defined, etc.
-                --  To jump back, press <C-t>.
-                map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            { "williamboman/mason.nvim", config = true },
+            "williamboman/mason-lspconfig.nvim",
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
+            "j-hui/fidget.nvim",
+            'hrsh7th/cmp-nvim-lsp',
+        },
 
-                -- Find references for the word under your cursor.
-                map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+        config = function()
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("cerdas-lsp-attach", { clear = true }),
+                callback = function(event)
+                    local map = function(keys, func, desc)
+                        vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+                    end
 
-                -- Jump to the implementation of the word under your cursor.
-                --  Useful when your language has ways of declaring types without an actual implementation.
-                map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+                    -- Jump to the definition of the word under your cursor.
+                    --  This is where a variable was first declared, or where a function is defined, etc.
+                    --  To jump back, press <C-t>.
+                    map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
 
-                -- Jump to the type of the word under your cursor.
-                --  Useful when you"re not sure what type a variable is and you want to see
-                --  the definition of its *type*, not where it was *defined*.
-                map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+                    -- Find references for the word under your cursor.
+                    map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 
-                -- Fuzzy find all the symbols in your current document.
-                --  Symbols are things like variables, functions, types, etc.
-                map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+                    -- Jump to the implementation of the word under your cursor.
+                    --  Useful when your language has ways of declaring types without an actual implementation.
+                    map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 
-                -- Fuzzy find all the symbols in your current workspace.
-                --  Similar to document symbols, except searches over your entire project.
-                map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+                    -- Jump to the type of the word under your cursor.
+                    --  Useful when you"re not sure what type a variable is and you want to see
+                    --  the definition of its *type*, not where it was *defined*.
+                    map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
 
-                -- Rename the variable under your cursor.
-                --  Most Language Servers support renaming across files, etc.
-                map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+                    -- Fuzzy find all the symbols in your current document.
+                    --  Symbols are things like variables, functions, types, etc.
+                    map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
 
-                -- Execute a code action, usually your cursor needs to be on top of an error
-                -- or a suggestion from your LSP for this to activate.
-                map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+                    -- Fuzzy find all the symbols in your current workspace.
+                    --  Similar to document symbols, except searches over your entire project.
+                    map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
-                -- Opens a popup that displays documentation about the word under your cursor
-                --  See `:help K` for why this keymap.
-                map("K", vim.lsp.buf.hover, "Hover Documentation")
-            end,
-        })
+                    -- Rename the variable under your cursor.
+                    --  Most Language Servers support renaming across files, etc.
+                    map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities = vim.tbl_deep_extend(
-            "force",
-            capabilities,
-            require("cmp_nvim_lsp").default_capabilities()
-        )
+                    -- Execute a code action, usually your cursor needs to be on top of an error
+                    -- or a suggestion from your LSP for this to activate.
+                    map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
-        require("fidget").setup({})
-        require("mason").setup({})
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                -- "black",  -- manual install using MasonInstall or install from Mason
-                -- "mypy",  -- manual install using MasonInstall or install from Mason
+                    -- Opens a popup that displays documentation about the word under your cursor
+                    --  See `:help K` for why this keymap.
+                    map("K", vim.lsp.buf.hover, "Hover Documentation")
+
+                    local client = vim.lsp.get_client_by_id(event.data.client_id)
+                    -- Toggle inlay hint
+                    if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+                        map("<leader>th", function()
+                            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+                        end, "[T]oggle Inlay [H]ints")
+                    end
+
+                end,
+            })
+
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = vim.tbl_deep_extend(
+                "force",
+                capabilities,
+                require("cmp_nvim_lsp").default_capabilities()
+            )
+
+            local servers = {
+                pyright = {},
+                lua_ls = {
+                    settings = {
+                        Lua = {
+                            completion = {
+                                callSnippet = "Replace",
+                            },
+                        },
+                    },
+                },
+            }
+
+            require("fidget").setup({})
+            require("mason").setup({})
+
+            local ensure_installed = vim.tbl_keys(servers or {})
+            vim.list_extend(ensure_installed, {
+                "lua_ls",
                 "pyright",
                 "ruff",
                 "html",
                 "cssls",
-                "lua_ls",
-            },
+            })
 
-            handlers = {
-                lua_ls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                completion = {
-                                    callSnippet = "Replace",
-                                },
-                                diagnostics = {
-                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    }
-                end,
-
-                function(server_name)
-                    require("lspconfig")[server_name].setup({
-                        capabilities = capabilities
-                    })
-                end,
-            },
-        })
-    end,
+            require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+            require("mason-lspconfig").setup({
+                handlers = {
+                    function(server_name)
+                        local server = servers[server_name] or {}
+                        server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+                        require("lspconfig")[server_name].setup(server)
+                    end,
+                },
+            })
+        end,
+    }
 }
