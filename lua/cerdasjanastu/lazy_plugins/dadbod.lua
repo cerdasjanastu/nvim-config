ef8e45219c535c8516ed15acc3580c7297b3e3a1
cerdasjanastu <cerdasjanastu@gmail.com>
2025-03-15 16:50:42 +0800

    feat: change the annoying <C-c> keymap in dadbod

--- lua/cerdasjanastu/lazy_plugins/dadbod.lua
+++ lua/cerdasjanastu/lazy_plugins/dadbod.lua
@@ -18,5 +18,9 @@ return {
         "DBUIFindBuffer",
     },
     init = function()
+        vim.g.db_ui_use_nerd_fonts = 1
+
+        -- changing the annoying default <C-c> in ftp sql
+        vim.g.ftplugin_sql_omni_key = "<C-0>"
     end,
 }
